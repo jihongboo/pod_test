2f2b773029213ba65fd1619f5329de2ba4a9298e
纪洪波 <jihongboo@qq.com>
2017-07-28 16:35:57 +0800

    init

---
+++ PodTest/PodTest.h
@@ -0,0 +1,13 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by 纪洪波 on 2017/7/28.
+//  Copyright © 2017年 纪洪波. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
++(void)test;
+@end
---
+++ PodTest/PodTest.m
@@ -0,0 +1,15 @@
+//
+//  PodTest.m
+//  PodTest
+//
+//  Created by 纪洪波 on 2017/7/28.
+//  Copyright © 2017年 纪洪波. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
++(void)test {
+    NSLog(@"hello world");
+}
+@end
